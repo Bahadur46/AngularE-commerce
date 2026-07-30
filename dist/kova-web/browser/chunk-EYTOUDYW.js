0573771dcdfc74915b2e1bf535910794a0863401
bahadur3028 <bahadur3028@gmail.com>
2026-07-30 17:16:35 +0530

    change

--- dist/kova-web/browser/chunk-EYTOUDYW.js
+++ dist/kova-web/browser/chunk-EYTOUDYW.js
@@ -1,1 +1,1 @@
-var o={production:!1,apiUrl:"https://localhost:7058/api",currency:"INR",locale:"en-IN"};export{o as a};
+var o={production:!1,apiUrl:"chatapi23-dyh9fjdygdeudwf7.centralindia-01.azurewebsites.net/api",currency:"INR",locale:"en-IN"};export{o as a};

4657754708f36a88340c0d7cc07b596f902faa70
bahadur3028 <bahadur3028@gmail.com>
2026-07-30 17:24:54 +0530

    change

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7058chatapi23-dyh9fjdygdeudwf7.centralindia-01.azurewebsites.net/api',
+  apiUrl: 'chatapi23-dyh9fjdygdeudwf7.centralindia-01.azurewebsites.net/api',
   currency: 'INR',
   locale: 'en-IN'
 };

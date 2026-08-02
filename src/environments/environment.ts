a159574618ebecc7bb563543fd8e537062ad96f7
bahadur3028 <bahadur3028@gmail.com>
2026-08-02 22:24:25 +0530

    changenew

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://johnkumar-001-site1.etempurl.com/api',
+  apiUrl: 'https://johnkumar-001-site1.etempurl.com/api',//live
   currency: 'INR',
   locale: 'en-IN'
 };
